9ce29075a96036b7caaaf13be16f35d9b96a2fee
VojtasekP <vojtasek.p@gmail.com>
2024-12-14 16:47:40 +0100

    deletion of 00clip model and 00yolo model folders

--- classification_pipeline/00YOLO_model/models/YOLO_model.py
+++
@@ -1,8 +0,0 @@
-import abc
-from ultralytics import YOLO
-
-
-class YoloModel(abc.ABC):
-    def __init__(self, path_to_model:str = None):
-        if not path_to_model:
-            self.model = YOLO("models/for_vegetables.pt")
